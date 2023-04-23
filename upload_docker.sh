ed3fc3bbdd23695456f168751f421e22183433af
Tuan Pham <tuanpa2295@gmail.com>
2023-04-23 22:53:57 +0700

    Add tasks

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=phamanhtuan221995/ml-api
 
-# Step 2:  
+# Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login -u mithie
+docker tag ml-api $dockerpath:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
